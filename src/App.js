cbcb6b2faf8b29556a4c4e9fb5d7bf78229b194c
Udaceue <rjemik3@gmail.com>
2022-05-03 13:09:01 +0200

    Changing project for football tips

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-import Menu from "./Menu";
-import Categories from "/.Categories";
-import items from "./data";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Elo</header>
+      <main className="App-header">
+        <h1>Siema</h1>
+      </main>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+/>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
